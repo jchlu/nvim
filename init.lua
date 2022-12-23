c95ff75a7671e9508648408b3ac0d435880a77b2
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2022-12-22 21:29:22 -0400

    Adds colorizer on css rgba() functions

--- init.lua
+++ init.lua
@@ -462,5 +462,10 @@ cmp.setup {
   },
 }
 
+require 'colorizer'.setup {
+  '*'; -- Highlight all files, but customize some others.
+  css = { rgb_fn = true; }; -- Enable parsing rgb(...) functions in css.
+}
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
