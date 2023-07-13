d1c6bd6b2ed69cf819c6832b07a819f37c04e5a4
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2023-07-13 11:41:29 -0400

    Install pyright for Python files

--- init.lua
+++ init.lua
@@ -439,7 +439,7 @@ require('mason').setup()
 
 -- Enable the following language servers
 -- Feel free to add/remove any LSPs that you want here. They will automatically be installed
-local servers = { 'tailwindcss', 'tsserver', 'cssls', 'lua_ls' }
+local servers = { 'tailwindcss', 'tsserver', 'cssls', 'lua_ls', 'pyright' }
 
 -- Ensure the servers above are installed
 require('mason-lspconfig').setup {
