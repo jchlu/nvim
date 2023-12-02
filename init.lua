78004c2d0db48359f9a3d9f78f9797aba3af13da
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2023-12-02 12:54:40 -0400

    Migrate from Packer to Lazy plugin manager

--- init.lua
+++ init.lua
@@ -1,17 +1,23 @@
--- Install packer
-local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-local is_bootstrap = false
-vim.g.loaded_python3_provider = 0
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  is_bootstrap = true
-  vim.fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
-  vim.cmd [[packadd packer.nvim]]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
-require('packer').startup(function(use)
+require('lazy').setup({
   -- Package manager
-  use 'wbthomason/packer.nvim'
-  use { "catppuccin/nvim", as = "catppuccin",
+  {
+    "catppuccin/nvim",
+    as = "catppuccin",
     config = function()
       require("catppuccin").setup({
         integrations = {
@@ -21,14 +27,16 @@ require('packer').startup(function(use)
       })
       vim.cmd [[colorscheme catppuccin-macchiato]] -- " catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
     end
-  }
-  use 'christoomey/vim-tmux-navigator'
-  use { 'folke/neodev.nvim',
+  },
+  'christoomey/vim-tmux-navigator',
+  {
+    'folke/neodev.nvim',
     config = function() require("neodev").setup() end
-  }
-  -- use 'mattn/emmet-vim'
-  -- use 'christoomey/vim-tmux-navigator'
-  use { 'norcalli/nvim-colorizer.lua',
+  },
+  -- 'mattn/emmet-vim',
+  -- 'christoomey/vim-tmux-navigator',
+  {
+    'norcalli/nvim-colorizer.lua',
     config = function()
       -- Use the `default_options` as the second parameter, which uses
       -- `foreground` for every mode. This is the inverse of the previous
@@ -40,9 +48,9 @@ require('packer').startup(function(use)
         rgb_fn = true,
       })
     end
-  }
-  use 'amadeus/vim-convert-color-to'
-  use 'mbbill/undotree'
+  },
+  'amadeus/vim-convert-color-to',
+  'mbbill/undotree',
   --[[ use { 'folke/tokyonight.nvim',
     config = function()
       require("tokyonight").setup({
@@ -51,26 +59,26 @@ require('packer').startup(function(use)
       })
     end
   } ]]
-  use 'tpope/vim-commentary'
-  use 'tpope/vim-surround'
-  use 'jchlu/lualine-time'
-  use {
+  'tpope/vim-commentary',
+  'tpope/vim-surround',
+  'jchlu/lualine-time',
+  {
     'nvim-tree/nvim-tree.lua',
-    requires = {
+    dependencies = {
       'nvim-tree/nvim-web-devicons', -- optional, for file icons
     },
     -- tag = 'nightly' -- optional, updated every week. (see issue #1193)
-    use {
+    {
       'notjedi/nvim-rooter.lua',
       config = function() require 'nvim-rooter'.setup() end
-    }
-  }
+    },
+  },
 
-  use { 'mg979/vim-visual-multi' }
+  { 'mg979/vim-visual-multi' },
 
-  use { -- LSP Configuration & Plugins
+  { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
-    requires = {
+    dependencies = {
       -- Automatically install LSPs to stdpath for neovim
       'williamboman/mason.nvim',
       'williamboman/mason-lspconfig.nvim',
@@ -78,39 +86,38 @@ require('packer').startup(function(use)
       -- Useful status updates for LSP
       'j-hui/fidget.nvim',
     },
-  }
-  -- use 'prettier/vim-prettier' -- Add Prettier
-  use { -- Autocompletion
+  },
+  -- 'prettier/vim-prettier', -- Add Prettier
+  { -- Autocompletion
     'hrsh7th/nvim-cmp',
-    requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
-  }
+    dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+  },
 
-  use { -- Highlight, edit, and navigate code
+  { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
-    run = function()
+    build = function()
       pcall(require('nvim-treesitter.install').update { with_sync = true })
     end,
-  }
-
-  use { -- Additional text objects via treesitter
-    'nvim-treesitter/nvim-treesitter-textobjects',
-    after = 'nvim-treesitter',
-  }
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    }
+  },
 
   -- Git related plugins
-  use {
+  {
     "kdheepak/lazygit.nvim",
     -- optional for floating window border decoration
-    requires = {
+    dependencies = {
       "nvim-lua/plenary.nvim",
     },
-  }
-  use 'j-morano/buffer_manager.nvim'
-  -- use 'tpope/vim-fugitive'
-  -- use 'tpope/vim-rhubarb'
-  use 'lewis6991/gitsigns.nvim'
+  },
+  'j-morano/buffer_manager.nvim',
+  -- 'tpope/vim-fugitive',
+  -- 'tpope/vim-rhubarb',
+  'lewis6991/gitsigns.nvim',
 
-  use { 'nvim-lualine/lualine.nvim', -- Fancier statusline
+  {
+    'nvim-lualine/lualine.nvim', -- Fancier statusline
     -- Set lualine as statusline
     -- See `:help lualine.txt`
     config = function()
@@ -126,26 +133,27 @@ require('packer').startup(function(use)
         }
       }
     end
-  }
-  -- use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
-  use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
-  use 'tpope/vim-sleuth'      -- Detect tabstop and shiftwidth automatically
+  },
+  -- 'lukas-reineke/indent-blankline.nvim', -- Add indentation guides even on blank lines
+  'numToStr/Comment.nvim', -- "gc" to comment visual regions/lines
+  'tpope/vim-sleuth',      -- Detect tabstop and shiftwidth automatically
 
   -- Fuzzy Finder (files, lsp, etc)
-  use { 'nvim-telescope/telescope.nvim',
+  {
+    'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
-    requires = {
+    dependencies = {
       'nvim-lua/plenary.nvim',
       "nvim-telescope/telescope-live-grep-args.nvim"
     },
     config = function()
       require("telescope").load_extension("live_grep_args")
     end
-  }
+  },
   -- A pretty list for showing diagnostics, references, telescope results, quickfix and location lists
-  use {
+  {
     "folke/trouble.nvim",
-    requires = "kyazdani42/nvim-web-devicons",
+    dependencies = "kyazdani42/nvim-web-devicons",
     config = function()
       require("trouble").setup {
         -- your configuration comes here
@@ -153,13 +161,13 @@ require('packer').startup(function(use)
         -- refer to the configuration section below
       }
     end
-  }
-  -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
-  use { 'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async' }
-  use "akinsho/toggleterm.nvim"
-  use "wuelnerdotexe/vim-astro"
-  use({
+  },
+  -- Fuzzy Finder Algorithm which dependencies local dependencies to be built. Only load if `make` is available
+  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make',                             cond = vim.fn.executable 'make' == 1 },
+  { 'kevinhwang91/nvim-ufo',                    dependencies = 'kevinhwang91/promise-async' },
+  'akinsho/toggleterm.nvim',
+  'wuelnerdotexe/vim-astro',
+  {
     "stevearc/conform.nvim",
     config = function()
       local conform = require("conform")
@@ -171,9 +179,9 @@ require('packer').startup(function(use)
         },
         formatters_by_ft = {
           -- lua = { "stylua" },
-          -- Conform will run multiple formatters sequentially
+          -- Conform will build multiple formatters sequentially
           -- python = { "isort", "black" },
-          -- Use a sub-list to run only the first available formatter
+          -- Use a sub-list to build only the first available formatter
           javascript = { "prettierd" },
           javascriptreact = { "prettierd" },
           typescriptreact = { "prettierd" },
@@ -205,37 +213,7 @@ require('packer').startup(function(use)
         })
       end, { desc = "Format file or range (in visual mode)" })
     end,
-  })
-  -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
-  local has_plugins, plugins = pcall(require, 'custom.plugins')
-  if has_plugins then
-    plugins(use)
-  end
-
-  if is_bootstrap then
-    require('packer').sync()
-  end
-end)
-
--- When we are bootstrapping a configuration, it doesn't
--- make sense to execute the rest of the init.lua.
---
--- You'll need to restart nvim, and then it will work.
-if is_bootstrap then
-  print '=================================='
-  print '    Plugins are being installed'
-  print '    Wait until Packer completes,'
-  print '       then restart nvim'
-  print '=================================='
-  return
-end
-
--- Automatically source and re-compile packer whenever you save this init.lua
-local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
-vim.api.nvim_create_autocmd('BufWritePost', {
-  command = 'source <afile> | PackerCompile',
-  group = packer_group,
-  pattern = vim.fn.expand '$MYVIMRC',
+  }
 })
 
 -- See `:help vim.highlight.on_yank()`
